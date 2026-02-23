9c83ecd58f92b57d8791e8782065d7c768099d26
dailaunch <dailaunch@gmail.com>
2026-02-23 17:32:41 +0800

    feat: add nextauth route

--- packages/dashboard/src/app/api/auth/[...nextauth]/route.ts
+++ packages/dashboard/src/app/api/auth/[...nextauth]/route.ts
@@ -8,11 +8,7 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    async session({ session, token }) {
-      return session
-    },
-  },
+  secret: process.env.NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
